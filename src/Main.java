bd301981c52cab2dae833b557c945dc9343de823
marzu <marzuqazam06@gmail.com>
2024-09-24 22:45:08 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int numOddEven = 3;
+
+        if (numOddEven % 2 == 0) {
+            System.out.println(numOddEven + " is Even.");
+        } else {
+            System.out.println(numOddEven + " is odd.");
+        }
+    }
+}
